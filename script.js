6bfa727ff39fb7e33472bb481c28ecab69759af4
ninekind <jazzlove2594@gmail.com>
2021-12-03 21:00:13 -0700

    mess

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function compute() {
-    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
+    var amount = interest + principal;
+
+    document.getElementById("result").innerHTML = "If you deposit '<mark>'" + principal + "'</mark>',\<br\>at an interest rate of '<mark>'" + rate + "'</mark>'%\<br\>You will receive an amount of '<mark>'" + amount + "'</mark>',\<br\>in the year '<mark>'" + year + "'</mark>'\<br\>"
 }
 
 function updateRate() {
